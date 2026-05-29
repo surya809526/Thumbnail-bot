1787afd0154d7ad5c00bb2d0d4dd12389584af30
surya809526 <swarajdubey605@gmail.com>
2026-05-29 11:56:36 +0530

    Update webhook URL in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw
 
 # Aapka bilkul naya fresh token yahan set kar diya hai
 TOKEN = "8658574106:AAHK-04fYQxC0u1H-ZcOWtxKf8bC_cuKYyY"
-WEBHOOK_URL = "https://thumbnail-bot-1.onrender.com"
+WEBHOOK_URL = "https://thumbnail-bot-ljn8.onrender.com"
 
 app = Flask(__name__)
 
